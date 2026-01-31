a2281257684401c8fd8be91f41ce3e95970dc638
di-shykh <di49@mail.ru>
2026-01-31 22:58:45 +0300

    init: start to do security

---
+++ src/securityDevices/routes/security.router.ts
@@ -0,0 +1,9 @@
+import {Router} from "express";
+
+export const securityRouter: Router = Router({});
+
+securityRouter
+    .get(
+        "/devices",
+
+    )
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -2,12 +2,21 @@ import express, { Express } from 'express';
 import { blogsRouter } from './blogs/routes/blogs.router';
 import { postsRouter } from './posts/routes/posts.router';
 import { testingRouter } from './testing/routes/testing.router';
-import {POSTS_PATH, BLOGS_PATH, TESTING_PATH, AUTH_PATH, USERS_PATH, COMMENTS_PATH} from "./core/paths/paths";
+import {
+    POSTS_PATH,
+    BLOGS_PATH,
+    TESTING_PATH,
+    AUTH_PATH,
+    USERS_PATH,
+    COMMENTS_PATH,
+    SECURITY_PATH
+} from "./core/paths/paths";
 import {HttpStatus} from "./core/types/http-statuses";
 import {usersRouter} from "./users/routes/user.router";
 import {authRouter} from "./auth/routes/auth.router";
 import {commentsRouter} from "./comments/routes/comments.router";
 import cookieParser from "cookie-parser";
+import {securityRouter} from "./securityDevices/routes/security.router";
 
 export const setupApp = (app: Express) => {
     app.use(express.json());
@@ -23,6 +32,7 @@ export const setupApp = (app: Express) => {
     app.use(AUTH_PATH, authRouter);
     app.use(USERS_PATH, usersRouter);
     app.use(COMMENTS_PATH, commentsRouter);
+    app.use(SECURITY_PATH, securityRouter);
 
 
     console.log('✅ Routers initialized:'); // 🔥
@@ -32,6 +42,7 @@ export const setupApp = (app: Express) => {
     console.log('- Auth:', AUTH_PATH);
     console.log('- Comments:', COMMENTS_PATH);
     console.log('- Testing:', TESTING_PATH);
+    console.log('- Security:', SECURITY_PATH);
 
     return app;
 }
