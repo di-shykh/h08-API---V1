b51cba8d648b8f82e8249137757e04244efa7394
di-shykh <di49@mail.ru>
2026-02-03 18:30:36 +0300

    feat: add delete devices/deviceId endpoint

---
+++ src/securityDevices/routes/handlers/delete-session.handler.ts
@@ -0,0 +1,44 @@
+import {Request, Response} from "express";
+import {errorHandler} from "../../../core/errors/error.handler";
+import {HttpStatus} from "../../../core/types/http-statuses";
+import {jwtService} from "../../../auth/application/jwt.service";
+import {securityService} from "../../application/security.services";
+import {ResultStatus} from "../../../core/result/result.code";
+import {resultCodeToHttpException} from "../../../core/result/resultCodeToHttpExeptions";
+import {Result} from "../../../core/result/result.type";
+
+export async function deleteSessionHandler(req: Request, res: Response) {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) {
+            return res.status(HttpStatus.Unauthorized).json({
+                errorsMessages: [{message: 'No refresh token'}]
+            });
+        }
+        const decodedPayload = await jwtService.verifyToken(refreshToken);
+        if (!decodedPayload || !decodedPayload.userId) {
+            return res.status(HttpStatus.Unauthorized).json({
+                errorsMessages: [{message: 'Refresh token is not valid'}]
+            });
+        }
+        const deviceId = req.params.id as string;
+        const result: Result = await securityService.deleteSessionByDeviceId(deviceId, decodedPayload.userId);
+        if (result.status===ResultStatus.Forbidden) {
+            res.status(resultCodeToHttpException(ResultStatus.Forbidden)).json({
+                errorsMessages: result.errorMessage
+            });
+            return;
+        }
+        if (result.status===ResultStatus.NotFound) {
+            res.status(resultCodeToHttpException(ResultStatus.NotFound)).json({
+                errorsMessages: result.errorMessage
+            });
+            return;
+        }
+        if(result.status===ResultStatus.NoContent) {
+            res.sendStatus(HttpStatus.NoContent);
+        }
+    } catch (e: unknown) {
+        errorHandler(e, res);
+    }
+}
--- src/securityDevices/routes/security.router.ts
+++ src/securityDevices/routes/security.router.ts
@@ -1,5 +1,7 @@
 import {Router} from "express";
 import {RefereshTokenGuard} from "../../auth/middlewares/refresh.token.guard";
+import {getSessionListHandler} from "./handlers/get-session-list.handler";
+import {deleteSessionHandler} from "./handlers/delete-session.handler";
 
 export const securityRouter: Router = Router({});
 
@@ -8,4 +10,9 @@ securityRouter
         "/devices",
         RefereshTokenGuard,
         getSessionListHandler
+    )
+    .delete(
+        "/devices/:id",
+        RefereshTokenGuard,
+        deleteSessionHandler
     )
