150258f62285c2d584bd35a593c62bd37a5d82a8
di-shykh <di49@mail.ru>
2026-02-05 22:35:47 +0300

    fix: fix bug in login user

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+declare global {
+    namespace Express {
+        export interface Request {
+            userId: string | null
+            deviceId: string | null
+        }
+    }
+}
+export {};
